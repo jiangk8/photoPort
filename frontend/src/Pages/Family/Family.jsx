e7e128d4f071580a0d1e26d86edd55ef773dc49c
Karen Jiang <kkarenjjiang.com>
2022-08-25 15:47:44 -0700

    Family

--- frontend/src/Pages/Family/Family.jsx
+++ frontend/src/Pages/Family/Family.jsx
@@ -4,10 +4,24 @@ import Gallery from "../Gallery/Gallery";
 import tempImg from "../../Images/Selfy.jpg";
 
 function Family() {
+  function importAll(r) {
+    let images = [];
+    r.keys().forEach((item) => {
+      images.push(r(item).default);
+    });
+    return images;
+  }
+
+  const images = importAll(
+    require.context("../../Images/Family/", false, /\.(png|jpe?g|svg)$/)
+  );
   return (
     <Container>
-      <Gallery img1={tempImg} img2={tempImg} img3={tempImg} />
-      <Gallery img1={tempImg} img2={tempImg} img3={tempImg} />
+      <Gallery img1={images[6]} />
+      <Gallery img1={images[7]} img2={images[3]} img3={images[2]} />
+      <Gallery img1={images[5]} />
+      <Gallery img1={images[1]} img2={images[4]} />
+      <Gallery img1={images[8]} />
     </Container>
   );
 }
